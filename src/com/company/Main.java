7322d485bebdfa41d51d0fa22ca079f905938d66
Mgbekwute Remigius Chidiebere <61621990+Chidoski1000@users.noreply.github.com>
2022-05-23 12:45:59 +0100

    updated file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,20 +4,13 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static String filename = "tasks.obj";
-
     public static void main(String[] args) {
         TodoList todoList = new TodoList();
 
-        //A string to hold the choice that will be entered by the user
-        String menuChoice = "-17";
+        String menuChoice = "-100";
 
         try {
             Scanner input = new Scanner(System.in);
-
-            // reading the date from task data file
-            // if this is the first time, a message will be shown that no data file is found
-            todoList.readFromFile(filename);
 
             Messages.showMessage("Welcome to ToDoList", false);
 
@@ -46,12 +39,10 @@ public class Main {
                 }
             }
 
-            todoList.saveToFile(filename);
 
         } catch (Exception e) {
             Messages.showMessage("UNCAUGHT EXCEPTION THROWN", true);
             System.out.println("Trying to write the unsaved data of all tasks in data file");
-            todoList.saveToFile(filename);
             System.out.println(e.getMessage());
             System.out.println(e.getStackTrace());
         }
